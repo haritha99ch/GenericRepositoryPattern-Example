b1dccb88e868c7ed4747658e60b36275303a734a
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-10-05 19:55:40 +0530

    Created ApplicationDbContext factory for migrations

---
+++ src/Example.Infrastructure/Configure.cs
@@ -0,0 +1,22 @@
+﻿using Example.AppSettings.Helpers;
+using Example.AppSettings.Options;
+using Example.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.Infrastructure;
+public static class Configure
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(UseSqlServerFromOptions);
+    }
+    
+    private static void UseSqlServerFromOptions(IServiceProvider serviceProvider, DbContextOptionsBuilder builder)
+    {
+        var sqlServerOptions = serviceProvider.GetOptions<SqlServerOptions>();
+        builder.UseSqlServer(sqlServerOptions.ConnectionString);
+        builder.EnableSensitiveDataLogging();
+        builder.EnableDetailedErrors();
+    }
+}
