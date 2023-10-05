28c12a4c2b48d46f88ecdee01ba506b5376105c8
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-10-05 16:42:21 +0530

    Created ApplicationDbContext

---
+++ src/Example.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Example.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example.Infrastructure.Contexts;
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; } = default!;
+    public DbSet<Member> Members { get; set; } = default!;
+    public DbSet<Blog> Blogs { get; set; } = default!;
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
