f9daf1a6324b160d255821170f5f27fb7c4d70cf
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-10-05 16:30:16 +0530

    Implemented domain rules and navigation props in Entities

--- src/Example.Domain/Entities/Account.cs
+++ src/Example.Domain/Entities/Account.cs
@@ -3,8 +3,12 @@
 namespace Example.Domain.Entities;
 public class Account : Entity
 {
+    [Required, EmailAddress]
     public required string Email { get; set; }
+    [Required]
     public required string Password { get; set; }
+    [Required]
     public required Role Role { get; set; }
+    [Required]
     public bool IsVerified { get; set; } = false;
 }
